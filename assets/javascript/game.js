9f3112aacef74829dd3bc7f1f05656eab953ce45
unknown <chugm1989@gmail.com>
2019-07-28 14:19:07 -0400

    completed mvp functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,8 +25,8 @@ instructBtn.onclick = function() {
   };
 };
 
-// Display userGuessLower
-var displayLetter = document.getElementById("pickedLetter");
+// getElementByIds
+
 
 // Function to compare user input to random generated number
 function checkLetter(letter, compPick) {
@@ -39,20 +39,35 @@ function checkLetter(letter, compPick) {
   };
 };
 
+
 document.onkeyup = function(event) {
   console.log(event);
+
 // GENERATE A RANDOM COMPUTER PICK FROM ALPHABET ARRAY
   var compPick = alphabet[Math.floor(Math.random() * alphabet.length)]; 
 
 //  Store keyup letter in userGuess variable
   var userGuess = event.key.toLowerCase();
 
-// Display guesesMade Array
-  displayLetter.innerHTML = "Guesses Made: " + guessesMade;
+// Display most recent guess
+  var displayLetter = document.getElementById("userGuess");
+  displayLetter.innerHTML = "Previous Guess " + userGuess; 
+
+  // Display guesesMade Array
+  var madeGuesses = document.getElementById("pickedLetter");
+  madeGuesses.innerHTML = "Guesses Made: " + guessesMade;
 
 // Display guessesRemaining
-var numRemaining = document.getElementById("remaining");
-numRemaining.innerHTML = "Number of Guesses Remaining " + guessesRemaining;
+  var numRemaining = document.getElementById("remaining");
+  numRemaining.innerHTML = "Number of Guesses Remaining " + guessesRemaining;
+
+// Display wins counter
+  var winsCounter = document.getElementById("winsCount");
+  winsCounter.innerHTML = "Wins " + wins;
+
+// Display losses counter
+  var lossCounter = document.getElementById("lossCount");
+  lossCounter.innerHTML = "Loses " + losses;
  
   checkLetter(userGuess, compPick);
   console.log(compPick);
